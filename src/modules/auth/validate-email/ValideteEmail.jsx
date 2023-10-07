5d255f849a8639d664c2d55f3d21c4777fbb2f5a
Hugo Moya <133818289+hema1019@users.noreply.github.com>
2023-10-06 22:37:03 -0600

    update in validateEmail

--- src/modules/auth/validate-email/ValideteEmail.jsx
+++ src/modules/auth/validate-email/ValideteEmail.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Box, Grid, Typography } from "@mui/material";
 import { useSearchParams } from "react-router-dom";
-import axios from "axios";
 import validate from "../../../assets/valid-email.png";
+import { request } from "../../../services/baseRequest";
 
 function ValidateEmail() {
   const [valid, setValid] = useState("");
@@ -17,8 +17,7 @@ function ValidateEmail() {
 
   useEffect(() => {
     if (token) {
-      axios
-        .post("http://localhost:8000/validate-user", { token })
+      request("post", "/api/v1/users/validate", { token })
         .then((res) => {
           console.log(res.status);
           setValid(res.status);
